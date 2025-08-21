1f37c10d603ca6315fd3bb2b1f4a5e8faee04bff
Sanith Bimsara <05sanith@gmail.com>
2025-08-21 20:54:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,6 @@ import pyrogram.utils
 from PIL import Image
 import threading, time, requests
 from logging_helper import LOGGER
-from plugins.comingsoon import register_plugin
 
 
 botStartTime = time.time()
@@ -96,13 +95,7 @@ async def SilentXBotz_start():
     bind_address = "0.0.0.0"
     await web.TCPSite(app, bind_address, PORT).start()
     await idle()
-    # ...
-    for name in files:
-        # ...
-        if plugin_name == "comingsoon":
-            import plugins.comingsoon
-            plugins.comingsoon.register_plugin(SilentX)
-    # ...
+
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
