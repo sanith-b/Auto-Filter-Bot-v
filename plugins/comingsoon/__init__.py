6aa57e03a6c8feb7ff2103cf5f4d4da20f87fed6
Sanith Bimsara <05sanith@gmail.com>
2025-08-21 20:54:18 +0530

    Delete plugins/comingsoon directory

--- plugins/comingsoon/__init__.py
+++
@@ -1,6 +0,0 @@
-from plugins.comingsoon.core import register_comingsoon
-from plugins.comingsoon.scheduler import start_scheduler
-
-def register_plugin(app):
-    register_comingsoon(app)
-    start_scheduler(app)
